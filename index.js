c5c193525fc082c6b3d350e67e97ef43fbb15311
Vinicius Wessner <vini.wessner@gmail.com>
2024-10-18 20:05:38 -0300

    adjust config

--- index.js
+++
@@ -1,9 +0,0 @@
-const { GarminConnect } = require('garmin-connect');
-// Create a new Garmin Connect Client
-const GCClient = new GarminConnect({
-    username: 'my.email@example.com',
-    password: 'MySecretPassword'
-});
-// Uses credentials from garmin.config.json or uses supplied params
-await GCClient.login();
-const userProfile = await GCClient.getUserProfile();
